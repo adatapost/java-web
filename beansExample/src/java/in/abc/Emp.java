13d19e3c454c0035a3c09573a19615a6af7e177b
adatapost <adatapost@gmail.com>
2013-07-18 13:37:26 +0530

    Added beansExample

---
+++ beansExample/src/java/in/abc/Emp.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.abc;
+
+/**
+ *
+ * @author admin
+ */
+public class Emp {
+    private int id;
+    private String name;
+
+    public Emp() {
+    }
+
+    public Emp(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ beansExample/web/index.jsp
@@ -0,0 +1,7 @@
+<jsp:useBean id="e1" class="in.abc.Emp"/>
+
+<jsp:setProperty name="e1"  property="id" value="10"/>
+<jsp:setProperty name="e1"  property="name" value="Rakesh"/>
+
+<jsp:getProperty name="e1" property="id"/>
+<jsp:getProperty name="e1" property="name"/>
